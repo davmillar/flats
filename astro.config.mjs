05a18fc363f11326b22c813bdb0542135e8b767e
David Millar <dave@davegoesthedistance.com>
2023-05-01 13:52:04 -0500

    Initial site structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://davmillar.github.io',
-    base: '/flats',
+  site: 'https://davmillar.github.io',
+  base: '/flats',
+  integrations: [mdx()]
 });
