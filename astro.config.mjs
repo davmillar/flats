04c10ee7edf5432af6714140236bb59ea2d1a10a
David Millar <dave@davegoesthedistance.com>
2023-05-08 09:52:42 -0500

    New pages and auto-includes for example components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
-
+import AutoImport from 'astro-auto-import';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://puzzlers.wtf',
-  integrations: [mdx()]
+  integrations: [
+    AutoImport({
+      imports: [
+        './src/components/Example.astro',
+        './src/components/Step.astro',
+        './src/components/Finally.astro',
+      ]
+    }),
+    mdx()
+  ]
 });
