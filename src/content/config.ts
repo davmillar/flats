8a84c7638144bb69894c9e4802cd98faa037e72c
David Millar <dave@davegoesthedistance.com>
2024-01-05 10:33:45 -0600

    Cleanup and inclusion of metadata.

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,9 @@ const defaultCollectionPattern = defineCollection({
     schema: z.object({
         title: z.string(),
         brief: z.string().optional(),
+        introducedBy: z.string().optional(),
+        introducedDate: z.date().optional(),
+        introducedDateText: z.string().optional(),
     }),
 });
 
